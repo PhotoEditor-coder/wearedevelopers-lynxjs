504b0da4015a50ecd47b495e0c683f73f1bdbd16
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2025-07-09 11:39:16 +0200

    Wire up the model

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,11 +2,23 @@ import './MainPage.css';
 
 import { Button } from '../components/Button.js';
 import { ListBox } from '../components/ListBox.js';
+import type { AppModel } from '../model/AppModel.js';
+import type { RemarkModel } from '../model/RemarkModel.js';
 
-export function MainPage() {
+export function MainPage(props: { appModel: AppModel }) {
+  const appModel = props.appModel;
+
+  const selectedItem: RemarkModel | undefined = appModel.selectedItem;
+
   return (
     <view className="mainPage">
-      <ListBox />
+      <ListBox
+        items={appModel.listItems()}
+        selectedItem={selectedItem}
+        onSelectItem={(listItem) => {
+          appModel.selectedItem = listItem;
+        }}
+      />
       <view
         style={{
           linearDirection: 'row',
