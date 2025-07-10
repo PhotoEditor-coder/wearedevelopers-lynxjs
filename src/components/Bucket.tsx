7b5fc2e49e257192070cbe0a9a0fe5d24698fe46
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2025-07-09 19:08:45 -0700

    Buckets show item count

--- src/components/Bucket.tsx
+++ src/components/Bucket.tsx
@@ -1,3 +1,5 @@
+import { useSyncExternalStore } from '@lynx-js/react';
+
 import type { BucketId } from '../model/RemarkModel.js';
 import type { AppModel } from '../model/AppModel.js';
 import './Bucket.css';
@@ -17,17 +19,26 @@ export function Bucket(props: BucketProps): JSX.Element {
   const appModel = props.appModel;
   const onClick = props.onClick;
 
+  useSyncExternalStore(
+    appModel.subscribeCallback,
+    // Rerender only if itemCount will change (i.e. the cached object is recreated)
+    () => appModel.listBucket(props.bucketId)
+  );
+
   const classNames: string[] = ['bucket'];
   if (props.highlightedBucketId === props.bucketId) {
     classNames.push('bucketSelected');
   }
+
+  const itemCount: number = appModel.listBucket(props.bucketId).length;
+  const label: string = `${props.label} (${itemCount})`;
 
   return (
     <view
       className={classNames.join(' ')}
       bindtouchstart={onClick ? () => onClick(props.bucketId) : undefined}
     >
-      <text className="bucketText">{props.label}</text>
+      <text className="bucketText">{label}</text>
     </view>
   );
 }
