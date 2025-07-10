da72d99b9271bd54963b347257dfcca6b36aa351
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2025-07-09 19:22:54 -0700

    Add EditPage

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,14 @@ import { root } from '@lynx-js/react';
 
 import { AppModel } from './model/AppModel.js';
 import { MainPage } from './pages/MainPage.js';
+import { EditPage } from './pages/EditPage.js';
 
 const appModel: AppModel = new AppModel();
 appModel.createRemark('not-ready', 'Example #1');
 appModel.createRemark('not-ready', 'Example #2');
 appModel.createRemark('not-ready', 'Example #3');
 appModel.createRemark('not-ready', 'Example #4');
-appModel.createRemark(
+const testRemark = appModel.createRemark(
   'ready',
   'TikTok Font',
   'TikTok Sans released on GitHub!'
@@ -23,7 +24,8 @@ appModel.createRemark(7, "What's coming in Q3");
 appModel.createRemark(7, 'Rust+JS', 'Review several new tools');
 appModel.createRemark(7, 'Share about the meetup');
 
-root.render(<MainPage appModel={appModel} />);
+// root.render(<MainPage appModel={appModel} />);
+root.render(<EditPage remarkModel={testRemark} />);
 
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept();
